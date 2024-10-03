b4e33bec629c449ef7f010505aed340c13a9f8ad
pjworsham <145511699+pjworsham@users.noreply.github.com>
2024-10-02 21:42:49 -0400

    update

---
+++ scripts/map.js
@@ -0,0 +1,5 @@
+const currentTemp = document.querySelector(`#current-temp`);
+const weatherIcon = document.querySelector(`weather-icon`);
+const captionDesc = document.querySelector(`figcaption`);
+
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=c54961ec375d33ae24655a5f694ccd3b"
