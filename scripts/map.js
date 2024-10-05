408a85eec1b732b722ec987dcc245cb5a72507d0
pjworsham <145511699+pjworsham@users.noreply.github.com>
2024-10-05 16:20:50 -0400

    update

--- scripts/map.js
+++ scripts/map.js
@@ -1,5 +1,39 @@
+
+// const town = document.querySelector(`#town`)
 const currentTemp = document.querySelector(`#current-temp`);
-const weatherIcon = document.querySelector(`weather-icon`);
+const weatherIcon = document.querySelector(`#weather-icon`);
 const captionDesc = document.querySelector(`figcaption`);
 
-const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=c54961ec375d33ae24655a5f694ccd3b"
+const myKey = "c54961ec375d33ae24655a5f694ccd3b"
+const myLat = "49.75"
+const myLong = "6.64"
+
+const url = `//api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&units=imperial&appid=${myKey}`
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+    function displayResults(data) {
+        currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+        const iconSrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+        let desc = data.weather[0].description;
+        weatherIcon.setAttribute('src', iconSrc);
+        weatherIcon.setAttribute('alt', data.weather[0].description);
+        captionDesc.textContent = `${desc}`;
+
+    }
+
+apiFetch();
