5fc24392f8cf28d087e0eea472446b520ddd7d8d
pjworsham <145511699+pjworsham@users.noreply.github.com>
2024-10-08 23:01:03 -0400

    update

--- chamber/scripts/home.js
+++ chamber/scripts/home.js
@@ -137,16 +137,67 @@ const weekday = [
     `Saturday`
 ]
 
+// //***********************************
+// Get member card
+// ************************************ */
+
+const memberUrl = "https://pjworsham.github.io/wdd231/chamber/data/members.json"
+
+const businessCard = document.querySelector(`#businessCard`);
+
+
+async function fetchBusinessCards() {
+    try {
+        const response = await fetch(memberUrl);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("unable to fetch business cards:", error);
+    }  
+} 
+
+async function displayBusinessCards() {
+    const data = await fetchBusinessCards();
+    let filteredData = data;
+
+    for (let i = filteredData.length; i > 3; i--) {
+        const randomIndex = Math.random() * filteredData.length;
+        filteredData.splice(randomIndex, 1);
+        console.log(filteredData);
+    }
 
 
 
+    // console.log(data);
+    createMemberCards(filteredData);
+}
+displayBusinessCards();
 
 
+function createMemberCards(data) {
+    const businessCard = document.querySelector(`#businessCard`);
+    businessCard.innerHTML = ``;
 
+    data.forEach(member => {
+        const card = document.createElement(`div`);
+        card.setAttribute(`id`, `businessCard1`);
 
+        card.innerHTML = `
+            <div class="cardHeader1">
+                <img src="${member.images}" alt="${member.title}" class="img1">
+                <div class="contactInfo1">
+                    <h3>${member.title}<br>${member.names}</h3>
+                    <p> ${member.address}</p>
+                    <p> ${member.phoneNumber}</p>
+                    <p> ${member.url}</p>
+                    <p>Membership Level: ${member.membershipLevel}</p>
+                </div>
+            </div>
+        `;
 
-
-
+        businessCard.appendChild(card);
+    });        
+}
 
 
 
