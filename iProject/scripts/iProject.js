8ff796c628e3d89292d2f37cd683ea25a16aa428
pjworsham <145511699+pjworsham@users.noreply.github.com>
2024-10-25 18:16:54 -0400

    update

---
+++ iProject/scripts/iProject.js
@@ -0,0 +1,16 @@
+let currentYear = document.querySelector("#currentYear");
+let lastModified = document.querySelector("#lastModified");
+const today = new Date();
+currentYear.textContent = today.getFullYear();
+lastModified.textContent = `Last Modification: ${document.lastModified}`;
+
+// **********************************
+// Code for hamburger button
+// ***********************************
+const hamburgerElement = document.querySelector('#myButton');
+const navElement = document.querySelector('.menuLinks');
+
+hamburgerElement.addEventListener('click', () => {
+    navElement.classList.toggle('open');
+    hamburgerElement.classList.toggle('open');
+});
