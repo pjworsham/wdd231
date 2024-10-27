940fb72e77e957abc91924f783742cd1c91cdff3
pjworsham <145511699+pjworsham@users.noreply.github.com>
2024-10-27 00:35:46 -0400

    updated

--- iProject/scripts/iProject.js
+++ iProject/scripts/iProject.js
@@ -15,63 +15,68 @@ hamburgerElement.addEventListener('click', () => {
     hamburgerElement.classList.toggle('open');
 });
 
-// // ***********************************
-// path to url (API) on Community Events page
-// ****************************************
-
-const myKey = "csAZxKkJtPr2yXak1avHGHoa4ATqXbps"
-
-
-const url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=csAZxKkJtPr2yXak1avHGHoa4ATqXbps&city=atlanta";
-
-async function getData() {
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-        }
-
-        const json = await response.json();
-        return json._embedded.events;
-    } catch (error) {
-        console.error(error.message);
-    }
-} 
-
+const communityMain = document.querySelector(`#communityMain`);
+if (communityMain) {
+    // // ***********************************
+    // path to url (API) on Community Events page
+    // ****************************************
     
-async function buildItems() {
-    const events = await getData();
-    const container = document.querySelector('.eventsContainer');
-    events.slice(0, 15).forEach(event => {
-        const eventItem = document.createElement('div');
-        eventItem.className = 'event-item';
-        eventItem.textContent = event.name;
-        eventItem.addEventListener('click', () => displayEventDetails(event));
-        container.appendChild(eventItem);
-    });
- }
-
- const eventInformation = document.querySelector(`#eventInformation`);
-
- function displayEventDetails(event) {
-    eventInformation.innerHTML = `
-        <button id="closeModal">❌</button>   
-        <h2>${event.name}</h2>
-        <p>${event.dates.start.localDate}</p>
-        <p>${event._embedded.venues[0].name}</p>
-        <p>${event.info || 'No additional information available.'}</p>
-    `
-    eventInformation.showModal();
-
-    const closeModal = document.querySelector("#closeModal");
-    closeModal.addEventListener("click", () => {
-        eventInformation.close(); 
-    });
- }
-         
-
-
-buildItems();
+    const myKey = "csAZxKkJtPr2yXak1avHGHoa4ATqXbps"
+    
+    
+    const url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=csAZxKkJtPr2yXak1avHGHoa4ATqXbps&city=atlanta";
+    
+    async function getData() {
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+    
+            const json = await response.json();
+            return json._embedded.events;
+        } catch (error) {
+            console.error(error.message);
+        }
+    } 
+    
+        
+    async function buildItems() {
+        const events = await getData();
+        const container = document.querySelector('.eventsContainer');
+    
+        events.slice(0, 15).forEach(event => {
+            const eventItem = document.createElement('div');
+            eventItem.className = 'event-item';
+            eventItem.innerHTML = `
+                <p>${event.name}</p>
+                <p>Click to see more</p>
+            `;
+            eventItem.addEventListener('click', () => displayEventDetails(event));
+            container.appendChild(eventItem);
+        });
+     }
+    
+     const eventInformation = document.querySelector(`#eventInformation`);
+    
+     function displayEventDetails(event) {
+        eventInformation.innerHTML = `
+            <button id="closeModal">❌</button>   
+            <h2>${event.name}</h2>
+            <p>${event.dates.start.localDate}</p>
+            <p>${event._embedded.venues[0].name}</p>
+            <p>${event.info || 'No additional information available.'}</p>
+        `
+        eventInformation.showModal();
+    
+        const closeModal = document.querySelector("#closeModal");
+        closeModal.addEventListener("click", () => {
+            eventInformation.close(); 
+        });
+     }
+             
+    buildItems();
+}
 
 // // *********************************
 // Local storage Resident resource page
@@ -95,4 +100,37 @@ if (welcomeMessage) {
     }
 
     localStorage.setItem('lastVisited', todayUTC);
+    
+}
+
+// // *****************************
+// Thank you Page
+// *********************************
+const urlData = window.location.href;
+const thankYouSelector = document.querySelector('#thankYou')
+const thanksData = document.createElement('div');
+
+if (thankYouSelector) {
+    const infoArray = urlData.split('?')[1].split('&');
+
+    function show(field) {
+        infoArray.forEach(element => {
+            if (element.startsWith(field)) {
+                result = element.split('=')[1];
+                result = result.replaceAll('%40', '@');
+                result = result.replaceAll('+', ' ');
+            }
+        });
+        return result
+    }
+
+    thanksData.innerHTML = `
+        <p><span class="thank-you-label">First Name: </span>${show("firstName")}</p>
+        <p><span class="thank-you-label">Last Name: </span>${show("lastName")}</p>
+        <p><span class="thank-you-label">Email: </span>${show("email")}</p>
+        <p><span class="thank-you-label">Phone Number: </span>${show("mobile")}</p>
+    `;
+
+    thankYouSelector.appendChild(thanksData);
+    localStorage.setItem('firstName', show("firstName"));
 }
